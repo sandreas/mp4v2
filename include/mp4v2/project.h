763254091ff7e755b81cff9663e52b814590e1e7
andreas <andreas.schroden@gmail.com>
2019-03-24 19:25:36 +0100

    fixed compilation error in rtphint.cpp

--- include/mp4v2/project.h
+++ include/mp4v2/project.h
@@ -24,7 +24,7 @@
 #define MP4V2_PROJECT_repo_rev        493
 #define MP4V2_PROJECT_repo_date       "2012-05-20 15:16:54 -0700 (Sun, 20 May 2012)"
 #define MP4V2_PROJECT_repo_type       "stable"
-#define MP4V2_PROJECT_build           "Tue Nov 22 10:10:06 EST 2016"
+#define MP4V2_PROJECT_build           "Sun Mar 24 16:23:51 CET 2019"
 
 /*****************************************************************************/
 
